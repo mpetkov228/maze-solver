7e77083775bc0710984882bad1ed5c742e7bb82c
mpetkov228 <mpetkov228@gmail.com>
2025-03-08 11:39:38 +0200

    Create Point and Line classes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from window import Window
+from graphics import Window, Line, Point
 
 
 def main():
+    line = Line(Point(3, 6), Point(500, 250))
     win = Window(800, 600)
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 
