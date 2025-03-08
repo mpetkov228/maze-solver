5a7b68d2ffeff96210129314e921bedfc16c8736
mpetkov228 <mpetkov228@gmail.com>
2025-03-08 16:50:53 +0200

    Create Cell class and draw some cells

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from graphics import Window, Line, Point
+from cell import Cell
 
 
 def main():
-    line = Line(Point(3, 6), Point(500, 250))
     win = Window(800, 600)
-    win.draw_line(line, "black")
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
     win.wait_for_close()
 
 
