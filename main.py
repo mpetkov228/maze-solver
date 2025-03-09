6c8394c945734d02f47934351b9972a497627d06
mpetkov228 <mpetkov228@gmail.com>
2025-03-09 12:58:12 +0200

    Make win parameter optional in Maze and Cell

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
-from graphics import Window, Line, Point
+from graphics import Window
 from maze import Maze
-from cell import Cell
 
 
 def main():
     win = Window(800, 600)
 
-    # c1 = Cell(win)
-    # c1.has_left_wall = False
-    # c1.draw(50, 50, 100, 100)
-
-    # c2 = Cell(win)
-    # c2.has_right_wall = False
-    # c2.draw(125, 125, 200, 200)
-
-    # c1.draw_move(c2, True)
-
     maze = Maze(50, 50, 20, 20, 10, 10, win)
-
 
     win.wait_for_close()
 
--- maze.py
+++ maze.py
@@ -3,7 +3,7 @@ from cell import Cell
 
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -13,10 +13,7 @@ class Maze:
         self._cell_size_y = cell_size_y
         self._win = win
         self._create_cells()
-        for i in range(self._num_rows):
-            for j in range(self._num_cols):
-                self._draw_cell(i, j)
-                
+
 
     def _create_cells(self):
         for i in range(self._num_rows):
@@ -25,9 +22,14 @@ class Maze:
                 cell = Cell(self._win)
                 col.append(cell)
             self._cells.append(col)
+        for i in range(self._num_rows):
+            for j in range(self._num_cols):
+                self._draw_cell(i, j)
 
     
     def _draw_cell(self, i, j):
+        if self._win is None:
+            return
         cell = self._cells[i][j]
         x1 = i * self._cell_size_x + self._x1
         y1 = j * self._cell_size_y + self._y1
@@ -38,5 +40,7 @@ class Maze:
 
 
     def _animate(self):
+        if self._win is None:
+            return
         self._win.redraw()
         time.sleep(0.05)
