32db1c5979fa70ac0c5af13fc9223afc4e629472
mpetkov228 <mpetkov228@gmail.com>
2025-03-10 13:08:13 +0200

    Add _break_entrance_and_exit method to Maze class

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     win = Window(800, 600)
 
     maze = Maze(50, 50, 20, 20, 20, 20, win)
+    maze._break_entrance_and_exit()
 
     win.wait_for_close()
 
--- test.py
+++ test.py
@@ -19,5 +19,16 @@ class Tests(unittest.TestCase):
         self.assertEqual(len(m._cells[0]), num_rows)
 
 
+    def test_maze_break_entrance_and_exit(self):
+        num_cols = 12
+        num_rows = 10
+        m = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m._break_entrance_and_exit()
+        top_left_cell = m._cells[0][0]
+        bottom_right_cell = m._cells[num_cols-1][num_rows-1]
+        self.assertFalse(top_left_cell.has_top_wall)
+        self.assertFalse(bottom_right_cell.has_bottom_wall)
+
+
 if __name__ == "__main__":
     unittest.main()
